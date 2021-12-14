099df40ff79dabb0e904ae86e76e5b085e1bbab8
paulotasso7 <paulotasso7@gmail.com>
2021-12-13 22:08:35 -0300

    uppercase const

--- pasta-de-exercicios/ex01/exercicio-sintax-e-operadores.js
+++ pasta-de-exercicios/ex01/exercicio-sintax-e-operadores.js
@@ -1,16 +1,16 @@
-const comparador = (num1, num2) => {
-    const n1 = num1;
-    const n2 = num2;
-    const soma = n1 + n2;
-    const confere10 = (soma >10) ?  `é maior que 10` :  `é menor que 10`
-    const confere20 = (soma <20) ?  `menor que 20` :  `maior que 20`
-    const igualdade = n1 === n2 ;
+const COMPARADOR = (num1, num2) => {
+    const N1 = num1;
+    const N2 = num2;
+    const SOMA = n1 + n2;
+    const CONFERE_10 = (SOMA >10) ?  `é maior que 10` :  `é menor que 10`
+    const CONFERE_20 = (SOMA <20) ?  `menor que 20` :  `maior que 20`
+    const IGUALDADE = n1 === n2 ;
 
-(igualdade) ? 
-        console.log(`Os números ${n1} e ${n2} são iguais. Sua soma é ${soma}, que ${confere10} e ${confere20}` ) 
-    :   console.log(`Os números ${n1} e ${n2} são diferentes. Sua soma é ${soma}, que ${confere10} e ${confere20}` )
+(IGUALDADE) ? 
+        console.log(`Os números ${N1} e ${N2} são iguais. Sua soma é ${SOMA}, que ${CONFERE_10} e ${CONFERE_20 }` ) 
+    :   console.log(`Os números ${N1} e ${N2} são diferentes. Sua soma é ${SOMA}, que ${CONFERE_10} e ${CONFERE_20 }` )
 
     
 }
 
-comparador(1, 11);
+COMPARADOR(1, 11);
