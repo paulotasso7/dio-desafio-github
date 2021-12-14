106a5a714664bbd921fad479e1e4d7bbad327e65
paulotasso7 <paulotasso7@gmail.com>
2021-12-13 21:56:35 -0300

    trocando var por const

--- pasta-de-exercicios/ex01/exercicio-sintax-e-operadores.js
+++ pasta-de-exercicios/ex01/exercicio-sintax-e-operadores.js
@@ -1,7 +1,7 @@
 const comparador = (num1, num2) => {
-    var n1 = num1;
-    var n2 = num2;
-    var soma = n1 + n2;
+    const n1 = num1;
+    const n2 = num2;
+    const soma = n1 + n2;
     const confere10 = (soma >10) ?  `é maior que 10` :  `é menor que 10`
     const confere20 = (soma <20) ?  `menor que 20` :  `maior que 20`
     const igualdade = n1 == n2 ;
