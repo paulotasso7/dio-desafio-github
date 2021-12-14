0e2118f88de1441b23a49ef76e7959f74d93fe0a
paulotasso7 <paulotasso7@gmail.com>
2021-12-13 22:23:44 -0300

    uppercasin variables

--- pasta-de-exercicios/ex01/exercicio-sintax-e-operadores.js
+++ pasta-de-exercicios/ex01/exercicio-sintax-e-operadores.js
@@ -1,10 +1,10 @@
 const COMPARADOR = (num1, num2) => {
     const N1 = num1;
     const N2 = num2;
-    const SOMA = n1 + n2;
+    const SOMA = N1 + N2;
     const CONFERE_10 = (SOMA >10) ?  `é maior que 10` :  `é menor que 10`
     const CONFERE_20 = (SOMA <20) ?  `menor que 20` :  `maior que 20`
-    const IGUALDADE = n1 === n2 ;
+    const IGUALDADE = N1 === N2 ;
 
 (IGUALDADE) ? 
         console.log(`Os números ${N1} e ${N2} são iguais. Sua soma é ${SOMA}, que ${CONFERE_10} e ${CONFERE_20 }` ) 
