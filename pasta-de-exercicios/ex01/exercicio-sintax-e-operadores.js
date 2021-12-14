cecc2b729a0e7f38b0773fc0c72474bf32cf8886
paulotasso7 <paulotasso7@gmail.com>
2021-12-13 22:03:38 -0300

    alterando operator de comparacao para identico

--- pasta-de-exercicios/ex01/exercicio-sintax-e-operadores.js
+++ pasta-de-exercicios/ex01/exercicio-sintax-e-operadores.js
@@ -4,7 +4,7 @@ const comparador = (num1, num2) => {
     const soma = n1 + n2;
     const confere10 = (soma >10) ?  `é maior que 10` :  `é menor que 10`
     const confere20 = (soma <20) ?  `menor que 20` :  `maior que 20`
-    const igualdade = n1 == n2 ;
+    const igualdade = n1 === n2 ;
 
 (igualdade) ? 
         console.log(`Os números ${n1} e ${n2} são iguais. Sua soma é ${soma}, que ${confere10} e ${confere20}` ) 
